130d8d8cc48f4f433e817b8ca7ce46d0678cec9d
ggajardo90 <ggajardo90@gmail.com>
2024-05-08 00:37:17 -0400

    configuracion login con usuario y email

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -6,12 +6,14 @@ use App\Actions\Fortify\CreateNewUser;
 use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
+use App\Models\User;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Laravel\Fortify\Fortify;
+use Illuminate\Support\Facades\Hash;
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -51,6 +53,16 @@ class FortifyServiceProvider extends ServiceProvider
 
         Fortify::verifyEmailView(function () {
             return view('auth.verify');
+        });
+
+        Fortify::authenticateUsing(function (Request $request) {
+            $user = User::where('email', $request->username)
+            ->orwhere('username',$request-> username)->first();
+     
+            if ($user &&
+                Hash::check($request->password, $user->password)) {
+                return $user;
+            }
         });
 
     }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,7 +26,7 @@
                             </div> --}}
                         </div>
                         <div class="card-body">
-                            <div class="bmd-form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
+                            {{-- <div class="bmd-form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
                                 <div class="input-group">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text">
@@ -41,6 +41,27 @@
                                     <div id="email-error" class="error text-danger pl-3" for="email"
                                         style="display: block;">
                                         <strong>{{ $errors->first('email') }}</strong>
+                                    </div>
+                                @endif
+                            </div> --}}
+
+                            {{-- Username --}}
+
+                            <div class="bmd-form-group{{ $errors->has('username') ? ' has-danger' : '' }}">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">
+                                            <i class="material-icons">face</i>
+                                        </span>
+                                    </div>
+                                    <input type="text" name="username" class="form-control"
+                                        placeholder="{{ __('username or email') }}" value="{{ old('username', null) }}" required
+                                        autocomplete="username" autofocus>
+                                </div>
+                                @if ($errors->has('username'))
+                                    <div id="username-error" class="error text-danger pl-3" for="username"
+                                        style="display: block;">
+                                        <strong>{{ $errors->first('username') }}</strong>
                                     </div>
                                 @endif
                             </div>
